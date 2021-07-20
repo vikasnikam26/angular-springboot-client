61cdaa6ee008e083e02f2c865deab022317e45bf
vikasnikam26 <vikas.nikam26@gmail.com>
2021-07-20 20:12:04 +0530

    Created employee edit and delete component

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee-service';
 
 @Component({
   selector: 'app-create-employee',
   templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  styleUrls: ['./create-employee.component.css'],
 })
 export class CreateEmployeeComponent implements OnInit {
+  employee: Employee = new Employee();
+  submitted = false;
+  constructor(
+    private employeeService: EmployeeService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  newEmployee(): void {
+    this.submitted = false;
+    this.employee = new Employee();
   }
 
+  save() {
+    this.employeeService.createEmployee(this.employee).subscribe(
+      (data) => {
+        console.log(data);
+        this.employee = new Employee();
+        this.gotoList();
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.save();
+  }
+
+  gotoList() {
+    this.router.navigate(['/employees']);
+  }
 }
